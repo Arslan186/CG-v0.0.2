adb1b886e21191125ac11af1a1e1bd9fa0f007fb
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-01 23:52:55 +0500

    Moved menu variable from menu.py to variable.py

--- variable.py
+++ variable.py
@@ -1,5 +1,5 @@
 from colored import fg, bg, attr
-
+#Variable COLOR
 ERROR = bg(196)
 OK = bg(26) + fg(52)
 DEFIS_COLOR = fg(88)
@@ -8,5 +8,12 @@ TEXT_COLOR_INPUT = bg(34) + fg(0)
 TEXT_COLOR_DIR = bg(240) + fg(0)
 RESET = attr(0)
 
-
+#Variable GENERAL
 DEFIS = '--------------------------------------------------------'
+
+MENU_OPTIONS = {
+        1: 'Укажите корневой каталог',
+        2: 'Укажите файлы и папки для очистки',
+        3: 'Очистить',
+        4: 'Выход',
+        }
