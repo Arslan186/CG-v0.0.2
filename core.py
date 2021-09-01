08321764523d15c86c16cf27149933bee1c1abcd
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-01 22:04:17 +0500

    modify core.py

--- core.py
+++ core.py
@@ -1,51 +1,39 @@
 import os
 import os.path
 import shutil
-from termcolor import colored
-
-#Добавить путь для удаления папок
-def write_way(text):
-    with open('/data/data/com.termux/files/home/storage/shared/CG-v0.0.2/way.txt', 'w', encoding='utf-8' ) as f:
-        try:
-            f.writelines('{0}\n'.format(text))
-        except TypeError:
-            print('Необходимо указать путь к корневому каталогу.\nПо умолчанию на андроид устройствах следующий путь:\n/data/data/com.termux/files/home/storage/shared')
+from variable import *
+from root_dir import *
+from add_directories import *
 
 
-#Добавить дириктррии для удаления
-def write_dir_del_folder(text):
-    with open('/data/data/com.termux/files/home/storage/shared/CG-v0.0.2/dir_del.txt', 'a', encoding='utf-8') as f:
-        try:
-            f.writelines('{0}\n'.format(text))
-        except TypeError:
-            print('Необходимо указать каталог или файл')
-
-#Склеить пути дирикторий
-def delet():
-    with open('/data/data/com.termux/files/home/storage/shared/CG-v0.0.2/way.txt', 'r', encoding='utf-8') as f:
-        for line_w in f:
-            result_w = line_w.replace('\n', '')
-    with open('/data/data/com.termux/files/home/storage/shared/CG-v0.0.2/dir_del.txt', 'r', encoding='utf-8') as f:
-        for line_d in f:
-            result_d = line_d.replace('\n', '')
-            result_dir = os.path.join(result_w, result_d)
-            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), result_dir)
+#Функция удаления файлов и папок
+def delet_file_dir():
+    with open('root_dir.txt', 'r', encoding='utf-8') as f:
+        for root_dir_r in f:
+            root_dir = root_dir_r.replace('\n', '')
+    with open('add_dir.txt', 'r', encoding='utf-8') as f:
+        for add_dir_r in f:
+            add_dir = add_dir_r.replace('\n', '')
+            result = os.path.join(root_dir, add_dir)
+            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), result)
             if os.path.isfile(path):
                 try:
                     os.unlink(path, dir_fd=None)
-                    print(path, colored("Ok", 'blue'))
+                    print('{}\n{}'.format(OK + 'Файл удален' + RESET, path))
+                    print(DEFIS)
                 except OSError as e:
-                    print(colored("Не требует очистки: %s : %s", 'magenta') % (path, e.strerror))
+                    print('{}\n{}'.format(ERROR + 'Файла не существует' + RESET, path))
+                    print(DEFIS)
             else:
                 try:
                     shutil.rmtree(path)
-                    print(path, colored("Ok", 'blue'))
+                    print('{}\n{}'.format(OK + 'Каталог удален' + RESET, path))
+                    print(DEFIS)
                 except OSError as e:
-                    print(colored("Не требует очистки: %s : %s", 'magenta') % (path, e.strerror))
-        print(colored("Очистка каталогов и файлов прошла успешно!", 'green'))
+                    print('{}\n{}'.format(ERROR + 'Каталог не требует очистки:' + RESET, path))
+                    print(DEFIS)
+        print(OK + "Очистка каталогов и файлов прошла успешно!" + RESET)
 
 
 if __name__ == '__main__':
-    write_dir_del_folder('test1\n' 'test2\n' 'test3\n')
-    write_way('set/set')
-    delet()
+    delet_file_dir()
