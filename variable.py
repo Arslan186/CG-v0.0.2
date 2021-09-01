75b69c8b1961a67d004784ff4dd3c1bd9a598800
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-01 23:34:50 +0500

    modify variable.py

--- variable.py
+++ variable.py
@@ -1,8 +1,12 @@
 from colored import fg, bg, attr
 
 ERROR = bg(196)
-OK = bg(10) + fg(0)
+OK = bg(26) + fg(52)
+DEFIS_COLOR = fg(88)
+TEXT_COLOR = fg(0)
+TEXT_COLOR_INPUT = bg(34) + fg(0)
 
 RESET = attr(0)
+
 
 DEFIS = '--------------------------------------------------------'
