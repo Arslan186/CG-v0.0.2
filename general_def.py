a92ed024a665f4c605da001fe0cbfa03fe37747d
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-02 19:00:55 +0500

    Moved function option_ok from option is general_def & Fixed Catalog Display

--- general_def.py
+++ general_def.py
@@ -3,3 +3,7 @@ from variable import *
 
 def print_defis():
     print('{:^10s}'.format(DEFIS_COLOR + DEFIS + RESET))
+
+
+def option_ok():
+    print(OK + 'Выполнено' + RESET)
--- option.py
+++ option.py
@@ -4,11 +4,6 @@ from add_directories import *
 from core import *
 from general_def import *
 
-
-def option_ok():
-    print(OK + 'Выполнено' + RESET)
-
-
 def option_root_dir_1():
     print(OK + 'По умолчанию корневой каталог на андроид устройствах:' + RESET)
     print(ERROR + '/data/data/com.termux/files/home/storage/shared' + RESET)
@@ -18,14 +13,13 @@ def option_root_dir_1():
 
 def option_add_dir_2():
     print(OK + 'Список файлов и каталогов для отслеживания:' + RESET)
-    with open('add_dir.txt', 'r', encoding='utf-8') as dir_txt_f:
-        dir_txt = ''
+    with open('add_dir.txt', 'r', encoding='utf-8') as f:
+        dir_txt_f = [dir_txt for dir_txt in f.read().split('\n') if dir_txt]
         for dir_txt in dir_txt_f:
             print(dir_txt)
             print_defis()
     print(ERROR + 'Для возврата в предыдущее меню используйте Ctrl+C' + RESET)
     add_directories(input(TEXT_COLOR_INPUT + 'Укажите файлы и папки для очистки: ' + RESET))
-    option_ok()
 
 
 def option_del_file_3():
