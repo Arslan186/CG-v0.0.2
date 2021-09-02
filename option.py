de3a0a2ec0673126b6c57be59e0ac55a6bc022f6
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-02 18:36:19 +0500

    add def print_defis

--- option.py
+++ option.py
@@ -2,6 +2,7 @@ from variable import *
 from root_dir import *
 from add_directories import *
 from core import *
+from general_def import *
 
 
 def option_ok():
@@ -21,6 +22,7 @@ def option_add_dir_2():
         dir_txt = ''
         for dir_txt in dir_txt_f:
             print(dir_txt)
+            print_defis()
     print(ERROR + 'Для возврата в предыдущее меню используйте Ctrl+C' + RESET)
     add_directories(input(TEXT_COLOR_INPUT + 'Укажите файлы и папки для очистки: ' + RESET))
     option_ok()
