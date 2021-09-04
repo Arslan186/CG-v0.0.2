b14c8c89563518bb8f47c02c310ee3a119151c8d
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-04 13:26:39 +0500

    Fixed the function option_add_dir_2, added try:,except:, finally

--- option.py
+++ option.py
@@ -12,13 +12,22 @@ def option_root_dir_1():
 
 
 def option_add_dir_2():
-    print(OK + 'Список файлов и каталогов для отслеживания:' + RESET)
-    with open(VAR_ADD, 'r', encoding='utf-8') as f:
-        dir_txt_f = [dir_txt for dir_txt in f.read().split('\n') if dir_txt]
+    dir_txt_f = ''
+    dir_txt = ''
+    try:
+        with open(VAR_ADD, 'r', encoding='utf-8') as f:
+            dir_txt_f = [dir_txt for dir_txt in f.read().split('\n') if dir_txt]
+            print(OK + 'Список файлов и каталогов для отслеживания:' + RESET)
+    except:
+        print_defis()
+        print(ERROR + 'Список файлов и каталогов для отслеживания не найден\nВведите данные' + RESET)
+        print_defis()
+    finally:
         for dir_txt in dir_txt_f:
             print(dir_txt)
             print_defis()
     print(ERROR + 'Для возврата в предыдущее меню используйте Ctrl+C' + RESET)
+    print_defis()
     add_directories(input(TEXT_COLOR_INPUT + 'Укажите файлы и папки для очистки: ' + RESET))
 
 
