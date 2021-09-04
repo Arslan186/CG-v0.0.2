ef278b46704a264f44f91037997c3207eff8071a
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-04 13:30:22 +0500

    added function print_defis()

--- add_directories.py
+++ add_directories.py
@@ -1,4 +1,5 @@
 from variable import *
+from general_def import *
 
 
 #Добавляем файлы и папки для очистки
@@ -7,6 +8,7 @@ def add_directories(text):
         if len(text) > 0:
             f.writelines('{0}\n'.format(text))
             print(OK + 'Файл или каталог успешно добавлен' + RESET)
+            print_defis()
         else:
             print(ERROR + 'Ошибка! Поле не может быть пустым' + RESET)
 
