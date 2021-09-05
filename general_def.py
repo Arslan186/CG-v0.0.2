7c3cf58474548073f7f85d98932713878220f59c
Arslan186 <arslan.hasanov.186@gmail.com>
2021-09-05 11:01:47 +0500

    addet function clearConsole()

--- general_def.py
+++ general_def.py
@@ -1,5 +1,5 @@
 from variable import *
-
+import os
 
 def print_defis():
     print('{:^10s}'.format(DEFIS_COLOR + DEFIS + RESET))
@@ -8,3 +8,5 @@ def print_defis():
 def option_ok():
     print(OK + 'Выполнено' + RESET)
     print_defis()
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
